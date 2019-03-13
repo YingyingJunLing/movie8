b3cf8978e724835c345590e223d9e86c2bbbc6ef
zhangbo1006 <744207346@qq.com>
2019-03-13 21:21:31 +0800

    first

---
+++ app/src/main/java/com/bw/movie/app/App.java
@@ -0,0 +1,10 @@
+package com.bw.movie.app;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
