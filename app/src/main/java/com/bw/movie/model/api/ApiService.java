c989eecff2fed3cbfc3537450e450602ea9cc3e1
zhangbo1006 <744207346@qq.com>
2019-03-14 18:34:37 +0800

    0314

--- app/src/main/java/com/bw/movie/model/api/ApiService.java
+++ app/src/main/java/com/bw/movie/model/api/ApiService.java
@@ -9,8 +9,7 @@ import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
 import retrofit2.http.Url;
 
-public interface ApiService
-{
+public interface ApiService {
     @POST
     Observable<LoginBean> login(@Url String url, @HeaderMap HashMap<String, String> hashMap);
 
--- app/src/main/java/com/bw/movie/presenter/MainPresenter.java
+++ app/src/main/java/com/bw/movie/presenter/MainPresenter.java
@@ -5,11 +5,6 @@ import com.bw.movie.base.BasePresenter;
 import com.bw.movie.model.api.ApiService;
 import com.bw.movie.model.utils.RetrofitUtils;
 
-/**
- * @Auther: 赵维鸣
- * @Date: 2019/3/2 10:38:15
- * @Description:
- */
 public class MainPresenter extends BasePresenter {
 
     private final RetrofitUtils retrofitUtils;
