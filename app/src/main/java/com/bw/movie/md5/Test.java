f4db8ae837846488b06019545124df491f97a093
zhangbo1006 <744207346@qq.com>
2019-03-27 10:27:30 +0800

    删除

--- app/src/main/java/com/bw/movie/md5/Test.java
+++
@@ -1,9 +0,0 @@
-package com.bw.movie.md5;
-
-public class Test
-{
-    public static void main(String[] args) {
-        String s = Md5.MD5("4607063movie");
-        System.out.println(s);
-    }
-}
