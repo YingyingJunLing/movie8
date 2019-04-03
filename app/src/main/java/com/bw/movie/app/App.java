58f3bedceab072820a2c94fb7fe38973e992efb7
zhangbo1006 <744207346@qq.com>
2019-04-03 14:47:55 +0800

    0403 合并

--- app/src/main/java/com/bw/movie/app/App.java
+++ app/src/main/java/com/bw/movie/app/App.java
@@ -1,31 +1,11 @@
 package com.bw.movie.app;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.LinkProperties;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.NetworkRequest;
-import android.os.Build;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.bw.movie.mvp.view.base.BaseActivity;
 import com.facebook.cache.disk.DiskCacheConfig;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
-<<<<<<< HEAD
-import com.tencent.mm.opensdk.modelmsg.SendAuth;
-import com.tencent.mm.opensdk.openapi.IWXAPI;
-import com.tencent.mm.opensdk.openapi.WXAPIFactory;
-import com.umeng.analytics.MobclickAgent;
-=======
->>>>>>> ccffb9c5d608469e1182bfb34fc6e22dab6a4dff
 import com.umeng.commonsdk.UMConfigure;
 
 import java.io.File;
@@ -57,9 +37,4 @@ public class App extends Application {
         UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "1fe6a20054bcef865eeb0991ee84525b");
 
     }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> ccffb9c5d608469e1182bfb34fc6e22dab6a4dff
 }
